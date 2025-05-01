2083907d593545a0704612da348882cd10330522
mjklaasen <mark.klaasen@gmail.com>
2025-05-01 13:56:28 +0200

    Created API.Common project for common types and created first endpoint for InventoryManagement

---
+++ src/Inventory/ShelfBuddy.InventoryManagement.Application/EndpointExtensions.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using ShelfBuddy.API.Common;
+using ShelfBuddy.Contracts;
+
+namespace ShelfBuddy.InventoryManagement.Application;
+
+public static class EndpointExtensions
+{
+    public static void MapInventoryEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapPost("/inventory",
+                async ([FromBody] CreateInventory message, [FromServices] IRequestClient<CreateInventory> client) =>
+                {
+                    var response = await client.GetResponse<InventoryCreated, ErrorResponse>(message);
+                    return response switch
+                    {
+                        {Message: InventoryCreated created} => Results.CreatedAtRoute("GetInventory",
+                            new {id = created.Id}, new InventoryDto(created.Id, message.Name, message.UserId, [])),
+                        {Message: ErrorResponse errorResponse} => CustomResults.Problem(errorResponse.Errors),
+                        _ => Results.Problem("An unknown error occurred.")
+                    };
+                })
+            .WithName("CreateInventory");
+    }
+}
---
+++ src/ShelfBuddy.Contracts/Inventory/InventoryDto.cs
@@ -0,0 +1,3 @@
+﻿namespace ShelfBuddy.Contracts;
+
+public record InventoryDto(Guid? Id, string Name, Guid UserId, Dictionary<Guid, int> Products);
