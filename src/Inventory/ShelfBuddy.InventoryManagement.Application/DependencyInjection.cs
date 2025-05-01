227f5a86aa2152256a98678e87f62cfc3b9b378a
mjklaasen <mark.klaasen@gmail.com>
2025-05-01 13:46:24 +0200

    Added DI extensions to inventory application

---
+++ src/Inventory/ShelfBuddy.InventoryManagement.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Hosting;
+
+namespace ShelfBuddy.InventoryManagement.Application;
+
+public static class DependencyInjection
+{
+    public static IHostApplicationBuilder AddInventoryManagementApplication(this IHostApplicationBuilder builder)
+    {
+        return builder;
+    }
+
+    public static IBusRegistrationConfigurator AddInventoryManagementConsumers(this IBusRegistrationConfigurator configurator)
+    {
+        configurator.AddConsumersFromNamespaceContaining(typeof(DependencyInjection));
+        return configurator;
+    }
+}
